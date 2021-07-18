d5547bc04923ce1c9cf8b0da9844322c4444b9a7
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-18 09:24:49 -0700

    add images

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ConvertUSD from './js/self-simulator.js';
+import Conversation from './js/self-simulator.js';
 
